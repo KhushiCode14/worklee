30b4854584290727b3f0c35cb8a5738e9d32d753
khushi Gupta <khushikumarigupta463@gmail.com>
2024-12-19 19:07:51 +0530

    freelancer sidebar created

--- frontend/src/components/freelancer/FreelancerProfile.jsx
+++ frontend/src/components/freelancer/FreelancerProfile.jsx
@@ -3,11 +3,11 @@ import SidebarProfile from "./freelancerProfile/SidebarProfile";
 
 const FreelancerProfile = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 p-5 m-10 border-2 rounded-md md:grid-cols-4">
-      <header className="col-span-1 md:col-span-4">
+    <div className="grid grid-cols-1 gap-4  m-10  rounded-md md:grid-cols-4 border border-gray-700">
+      <header className="col-span-1 md:col-span-4    p-5 ">
         <HeaderProfile />
       </header>
-      <aside className="hidden col-span-1 border border-gray-700 md:block">
+      <aside className=" col-span-1  md:block">
         <SidebarProfile />
       </aside>
       <main className="col-span-1 md:col-span-3">
--- frontend/src/components/freelancer/freelancerProfile/SidebarProfile.jsx
+++ frontend/src/components/freelancer/freelancerProfile/SidebarProfile.jsx
@@ -3,22 +3,33 @@ import { GoPencil } from "react-icons/go";
 
 const SidebarProfile = () => {
   return (
-    <div className="h-auto ">
+    <div className="h-auto p-6 m-0 border-gray-500 border-t border-r w-full rounded-md">
       <div>
-        <div className="flex justify-between h-auto">
-          View Profile
+        {/* view profile */}
+        <div className="flex justify-between h-auto text-black items-center border-b border-gray-600 w-full p-5">
+          <p className="text-xl"> View Profile</p>
           <div className="flex gap-2 ">
-            <FaPlus />
-            <GoPencil />
+            <FaPlus
+              size={30}
+              color="green"
+              className="rounded-full border border-green-600 p-2"
+            />
+            <GoPencil
+              size={30}
+              color="green"
+              className="rounded-full border border-green-600 p-2"
+            />
           </div>
         </div>
-        <div className="flex gap-5">
-          <div>total earning</div>
-          <div>total jobs</div>
-          <div>total hours</div>
+        {/* earning hours */}
+        <div className="fl   border-gray-600 w-full p-5 border-b">
+          <div className="text-xl text-black">Total earning</div>
+          <div className="text-xl text-black">total jobs</div>
+          <div className="text-xl text-black">total hours</div>
         </div>
-        <div className="flex flex-col gap-2">
-          <div>Connects 5</div>
+        {/* connects related */}
+        <div className="flex   border-gray-600 w-full p-5 border-b flex-col gap-2">
+          <div className="text-xl text-black">Connects 5</div>
           <div>
             <button className="py-2 font-bold text-green-600">Connect</button>
             <button className="py-2 font-bold text-green-600 ">
@@ -27,8 +38,86 @@ const SidebarProfile = () => {
           </div>
         </div>
       </div>
-      <div></div>
-      <div></div>
+      {/* hour */}
+      <div className="flex justify-between h-auto  border-gray-600 w-full p-5 border-b">
+        <p className="text-black text-xl"> Hour per week</p>
+        <div className="flex gap-2 ">
+          <FaPlus
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+          <GoPencil
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+        </div>
+      </div>
+      {/* language */}
+      <div className="flex justify-between h-auto   border-gray-600 w-full p-5 border-b">
+        <p className="text-black text-xl"> Languages</p>
+        <div className="flex gap-2 ">
+          <FaPlus
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+          <GoPencil
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+        </div>
+      </div>
+      {/* verification */}
+      <div className="flex justify-between h-auto   border-gray-600 w-full p-5 border-b">
+        <p className="text-black text-xl"> Verification</p>
+        <div className="flex gap-2 ">
+          <FaPlus
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+          <GoPencil
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+        </div>
+      </div>
+      {/* education */}
+      <div className="flex justify-between h-auto   border-gray-600 w-full p-5 border-b">
+        <p className="text-black text-xl"> Education</p>
+        <div className="flex gap-2 ">
+          <FaPlus
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+          <GoPencil
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+        </div>
+      </div>
+      {/* links */}
+      <div className="flex justify-between h-auto   border-gray-600 w-full p-5 border-b">
+        <p className="text-black text-xl">Linked accounts </p>
+        <div className="flex gap-2 ">
+          <FaPlus
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+          <GoPencil
+            size={30}
+            color="green"
+            className="rounded-full border border-green-600 p-2"
+          />
+        </div>
+      </div>
     </div>
   );
 };
